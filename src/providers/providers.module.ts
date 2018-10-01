057c8d21111ac52542ed02beb4c8ebb7ae3f0cfe
Frost <matthias.prost@epitech.eu>
2018-10-01 11:14:42 +0900

    Added ionic unit test

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {ApiServiceModule} from "./api-service/api-service.module";
 import {AuthServiceProvider} from "./auth/auth-service";
 import {MirrorModule} from "./mirror/mirror.module";
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
   declarations: [],
   imports: [
-    ApiServiceModule
+    ApiServiceModule,
+    IonicStorageModule.forRoot()
   ],
   exports: [],
   providers: [

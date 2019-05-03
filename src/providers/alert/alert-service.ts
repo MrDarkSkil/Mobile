c759519ff2b0bb1556400eafaef977e5427b0c10
Frost <matthias.prost@epitech.eu>
2019-05-03 15:59:19 +0900

    Updated alert service

--- src/providers/alert/alert-service.ts
+++ src/providers/alert/alert-service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
-import { AlertController } from 'ionic-angular';
+import {Injectable} from '@angular/core';
+import {AlertController} from 'ionic-angular';
 
 @Injectable()
 export class AlertProvider {
 
-  constructor(public alertCtrl: AlertController) {}
+  constructor(public alertCtrl: AlertController) {
+  }
 
-  displayAlert(title: string, message: string) {
+  public displayAlert(title: string, message: string) {
     let confirm = this.alertCtrl.create({
       title: title,
       message: message,
@@ -14,21 +15,22 @@ export class AlertProvider {
       buttons: [
         {
           text: "OK",
-          handler: () => {
-            console.log('OK clicked');
-          }
         }
       ]
     });
     confirm.present();
   }
 
-  errorAlert(err: any) {
-    const text = Object.keys(err.error.error)[0];
+  public errorAlert(err: any) {
+    if (err === null || err === undefined || !err.error.error) {
+      this.displayAlert('Error', 'An error occurred!');
+    } else {
+      const text = Object.keys(err.error.error)[0];
 
-    const title = 'Error ' + err.status;
-    const message = err.error.error[text][0];
+      const title = 'Error ' + err.status;
+      const message = err.error.error[text][0];
 
-    this.displayAlert(title,message);
+      this.displayAlert(title, message);
+    }
   }
 }

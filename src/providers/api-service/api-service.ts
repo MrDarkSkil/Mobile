e948acbfb78d553543b6651a163ff8a1a81c50a2
Frost <matthias.prost@epitech.eu>
2019-05-08 12:32:00 +0900

    Deleted useless files from api

--- src/providers/api-service/api-service.ts
+++ src/providers/api-service/api-service.ts
@@ -1,9 +1,5 @@
 import {Injectable} from '@angular/core';
-import {GetService} from "./http/get-service";
-import {PostService} from "./http/post-service";
 import {Platform} from "ionic-angular";
-import {PutService} from "./http/put-service";
-import {DeleteService} from './http/delete-service';
 import {HttpClient} from '@angular/common/http';
 import {Storage} from "@ionic/storage";
 import {SplashScreen} from "@ionic-native/splash-screen/ngx";
@@ -20,9 +16,7 @@ export class ApiServiceProvider {
 
   private readonly apiUrl: string = '/api';
 
-  constructor(private platform: Platform, private getService: GetService,
-              private postService: PostService, private putService: PutService,
-              private deleteService: DeleteService, private httpClient: HttpClient, private storage: Storage,
+  constructor(private platform: Platform, private httpClient: HttpClient, private storage: Storage,
               private splashScreen: SplashScreen) {
 
     if (this.platform.is('cordova') == true) {
@@ -80,20 +74,4 @@ export class ApiServiceProvider {
   public getApiUrl() {
     return (this.apiUrl);
   }
-
-  /*public get<T>(url: string, token?: string): Promise<T> {
-    return (this.getService.submit<T>(url, token));
-  }
-
-  public post<T>(url: string, token?: string, body?: object): Promise<T> {
-    return (this.postService.submit<T>(url, token, body));
-  }
-
-  public put<T>(url: string, token?: string, body?: object): Promise<T> {
-    return (this.putService.submit<T>(url, token, body));
-  }
-
-  public delete<T>(url: string, token?: string): Promise<T> {
-    return (this.deleteService.submit<T>(url, token));
-  }*/
 }

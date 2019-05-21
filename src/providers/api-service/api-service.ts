8a549aa2d5c14f83e220901636296ea8684f9ffe
Frost <matthias.prost@epitech.eu>
2019-05-21 21:49:46 +0900

    Added splash screen + logo

--- src/providers/api-service/api-service.ts
+++ src/providers/api-service/api-service.ts
@@ -20,7 +20,7 @@ export class ApiServiceProvider {
               private splashScreen: SplashScreen, private app: App) {
 
     if (this.platform.is('cordova') == true) {
-      this.apiUrl = 'https://api.elios-mirror.com';
+      this.apiUrl = 'https://dev.elios-mirror.com';
     }
 
   }

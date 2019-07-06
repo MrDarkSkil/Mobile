d9eb0316ccdc49aa3e498000766ec728f741acfc
Frost <matthias.prost@epitech.eu>
2019-07-06 21:44:54 +0200

    Added all assets + basic login page + design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/pages/auth/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  public email: string = null;
+  public password: string = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

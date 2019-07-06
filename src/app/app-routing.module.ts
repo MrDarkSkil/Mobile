9d882cc662c5758a503422c371e2258988cbd400
Frost <matthias.prost@epitech.eu>
2019-07-06 22:03:22 +0200

    Added lost password page + updated design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' }
+  { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
+  { path: 'lost-password', loadChildren: './pages/auth/lost-password/lost-password.module#LostPasswordPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/pages/auth/lost-password/lost-password.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lost-password',
+  templateUrl: './lost-password.page.html',
+  styleUrls: ['./lost-password.page.scss'],
+})
+export class LostPasswordPage implements OnInit {
+
+  public email: string = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

b693f9a00bea9be3b3054db16eff36cdea3ed065
Frost <matthias.prost@epitech.eu>
2019-07-06 21:56:09 +0200

    Added register page + updated design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [

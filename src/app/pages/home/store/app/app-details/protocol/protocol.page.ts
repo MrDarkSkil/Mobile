6bd4d659b3005a26ceb503fa14d341ed5fdaf8bc
Frost <matthias.prost@epitech.eu>
2019-12-01 21:52:58 +0100

    Form update

--- src/app/pages/home/store/app/app-details/protocol/protocol.page.ts
+++ src/app/pages/home/store/app/app-details/protocol/protocol.page.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {ModalController, NavParams} from '@ionic/angular';
 import {ProtocolService} from '../../../../../../services/protocol/protocol.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-protocol',
@@ -10,20 +11,32 @@ import {ProtocolService} from '../../../../../../services/protocol/protocol.serv
 export class ProtocolPage {
 
   @Input() moduleId: string;
+  private dataToSend = new Map();
+  private todo: FormGroup;
   public values = [
     {
       type: 'toggle',
       name: 'show_media',
       placeholder: 'Voulez-vous afficher ce media ?',
       required: false,
-      values: true
+      values: true,
+      value: true,
+    },
+    {
+      type: 'toggle',
+      name: 'media',
+      placeholder: 'Voulez-vous afficher ce media ?',
+      required: false,
+      values: true,
+      value: false,
     },
     {
       type: 'input',
       name: 'email',
       placeholder: 'Votre adresse email',
       required: true,
-      values: true
+      values: true,
+      value: 'Kek'
     },
     {
       type: 'dropdown',
@@ -39,22 +52,35 @@ export class ProtocolPage {
           text: 'Option 2',
           value: 2
         }
-      ]
+      ],
+      value: 2
     }
   ];
 
-  constructor(private modalCtrl: ModalController, private navParams: NavParams, private protocolService: ProtocolService) {
+  constructor(private modalCtrl: ModalController, private navParams: NavParams,
+              private protocolService: ProtocolService, private formBuilder: FormBuilder) {
     const moduleId = navParams.get('moduleId');
 
     /*this.protocolService.getModuleForm(moduleId).then(result => {
       console.log('kek');
     });*/
+
+    this.todo = this.formBuilder.group({
+      value: [''],
+    });
   }
 
-  close() {
+  public close() {
     this.modalCtrl.dismiss({
       dismissed: true
     });
+  }
+
+  logForm() {
+    console.log(this.todo.value);
+  }
+
+  public sendData() {
   }
 
 }

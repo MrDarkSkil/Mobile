b94225c46ae3bb31e0fd84d4d8b01a5fc0ad28eb
Léo <leohub@live.fr>
2019-12-03 20:15:17 +0100

    Fix matthias bug

--- src/app/pages/home/store/app/app-details/protocol/protocol.page.ts
+++ src/app/pages/home/store/app/app-details/protocol/protocol.page.ts
@@ -1,7 +1,7 @@
-import {Component, Input} from '@angular/core';
-import {ModalController, NavParams} from '@ionic/angular';
-import {ProtocolService} from '../../../../../../services/protocol/protocol.service';
-import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { ProtocolService } from '../../../../../../services/protocol/protocol.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-protocol',
@@ -17,32 +17,31 @@ export class ProtocolPage {
   private readonly mirrorId;
 
   constructor(private modalCtrl: ModalController, private navParams: NavParams,
-              private protocolService: ProtocolService, private formBuilder: FormBuilder) {
+    private protocolService: ProtocolService, private formBuilder: FormBuilder) {
     const moduleId = navParams.get('moduleId');
     this.mirrorId = navParams.get('mirrorId');
 
     this.protocolService.getModuleForm(this.mirrorId, moduleId).then(result => {
       this.inputs = result;
+      const object = {};
+
+      this.inputs.forEach((input) => {
+        if (input.type === 'toggle') {
+          object[input.name] = new FormControl(input.value ? input.value : false);
+        } else if (input.type === 'input') {
+          object[input.name] = new FormControl(input.value ? input.value : '');
+        } else if (input.type === 'dropdown') {
+          object[input.name] = new FormControl(input.value ? input.value : '0');
+        } else {
+          object[input.name] = new FormControl(input.value ? input.value : '');
+        }
+      });
+
+
+      this.todo = this.formBuilder.group(object);
     }).catch(error => {
       console.log('PROTOCOL ERROR:', error);
     });
-
-    const object = {};
-
-    this.inputs.forEach((input) => {
-      if (input.type === 'toggle') {
-        object[input.name] = new FormControl(input.value ? input.value : false);
-      } else if (input.type === 'input') {
-        object[input.name] = new FormControl(input.value ? input.value : '');
-      } else if (input.type === 'dropdown') {
-        object[input.name] = new FormControl(input.value ? input.value : '0');
-      } else {
-        object[input.name] = new FormControl(input.value ? input.value : '');
-      }
-    });
-
-
-    this.todo = this.formBuilder.group(object);
   }
 
   public close() {
